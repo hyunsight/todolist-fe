3484f49bac7ac7ad963b5682f324b601dddc1af7
hyunsight <blueskytjgus@gmail.com>
2024-05-22 19:56:50 +0900

    todolist-fe #1

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React from 'react'
+import TodoItem from './TodoItem'
 
-const TodoBoard = () => {
-  return (
-    <div>
-      <h2>Todo List</h2>
-      {/* <TodoItem/> will be here once we get the todoList */}
-      <h2>There is no Item to show</h2>
-    </div>
-  );
-};
+const TodoBoard = ({ todoList, updateTask, deleteTask }) => {
+   return (
+      <div>
+         <h2>Todo List</h2>
+         {todoList.length > 0 ? (
+            todoList.map((item, index) => (
+               <TodoItem
+                  item={item}
+                  key={index}
+                  updateTask={updateTask}
+                  deleteTask={deleteTask}
+               />
+            ))
+         ) : (
+            <h2>There is no Item to show</h2>
+         )}
+         {/* <TodoItem/> will be here once we get the todoList */}
+      </div>
+   )
+}
 
-export default TodoBoard;
+export default TodoBoard
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,33 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
 
-const TodoItem = () => {
-  return (
-    <Row>
-      <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+const TodoItem = ({ item, updateTask, deleteTask }) => {
+   return (
+      <Row>
+         <Col xs={12}>
+            <div
+               className={`todo-item ${item.isComplete ? 'item-complete' : ''}`}
+            >
+               <div className="todo-content">{item.task}</div>
 
-          <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
-          </div>
-        </div>
-      </Col>
-    </Row>
-  );
-};
+               <div>
+                  <button
+                     className="button-delete"
+                     onClick={() => deleteTask(item._id)}
+                  >
+                     삭제
+                  </button>
+                  <button
+                     className="button-delete"
+                     onClick={() => updateTask(item._id)}
+                  >
+                     {item.isComplete ? '안끝남' : '끝남'}
+                  </button>
+               </div>
+            </div>
+         </Col>
+      </Row>
+   )
+}
 
-export default TodoItem;
+export default TodoItem
